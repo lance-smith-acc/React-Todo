35318b238f21e2c02d33e0028f700a4be22adbe0
Lance Smith <57012297+lance-smith-acc@users.noreply.github.com>
2020-01-13 17:21:34 -0600

    completed project functionality

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoItem = props => {
+    return (
+        <div className={`item${props.item.completed ? " completed" : ""}`} onClick={() => props.completeItem(props.item.id)}>
+            <p>{props.item.name}</p>
+        </div>
+    )
+}
+
+export default TodoItem;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import TodoItem from './Todo'
+
+const TodoList = props => {
+    return (
+            <div class='todo-list'>
+               <h4>To Do List</h4>
+                <ul>
+                    {props.todoItems.map(item => (
+                        <TodoItem key={item.id} item={item} completeItem={props.completeItem} />
+                    ))}
+                </ul>    
+            </div>
+        );
+ 
+}
+
+export default TodoList;
